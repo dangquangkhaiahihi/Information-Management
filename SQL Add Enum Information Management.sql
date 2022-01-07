036c98c1b79147d09ddf92f3d4d00b747d925723
dangquangkhaiahihi <42770800+dangquangkhaiahihi@users.noreply.github.com>
2022-01-07 16:52:01 +0700

    Enum data for SQL

---
+++ SQL Add Enum Information Management.sql
@@ -0,0 +1,15 @@
+INSERT INTO `informationmanagement`.`position`(`position`)VALUES('Owner');
+INSERT INTO `informationmanagement`.`position`(`position`)VALUES('Manager');
+
+INSERT INTO `informationmanagement`.`provider`(`name`)VALUES('Cuc an toan thuc pham');
+INSERT INTO `informationmanagement`.`provider`(`name`)VALUES('Quan');
+
+INSERT INTO `informationmanagement`.`license_type`(`type`)VALUES('Giay phep kinh doanh');
+INSERT INTO `informationmanagement`.`license_type`(`type`)VALUES('Chung nhan ve sinh an toan thuc pham');
+
+INSERT INTO `informationmanagement`.`role`(`role`)VALUES('ADMIN');
+INSERT INTO `informationmanagement`.`role`(`role`)VALUES('STAFF');
+
+INSERT INTO `informationmanagement`.`status`(`status`)VALUES('ACTIVE');
+INSERT INTO `informationmanagement`.`status`(`status`)VALUES('INACTIVE');
+
